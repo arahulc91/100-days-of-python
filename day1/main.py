90b7e2812142a97bbf359aa420ecc63302d49973
Alvin Rahul Chauhan <arahulc91@gmail.com>
2025-03-03 14:47:08 +1200

    day 1 project completed

--- day1/main.py
+++ day1/main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+#Day 1: Band Name Generator
+print("Welcome to the Band Name Generator.")
+print("What's the name of the city you grew up in?")
+city = input()
+print("What's your pet's name?")
+pet = input()
+print(f"Your band name could be {city} {pet}")
